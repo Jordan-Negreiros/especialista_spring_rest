f9c12b2056c25a998c115d3222bfe31a42e667b0
Jordan-Negreiros <jordannegreirossantos@gmail.com>
2021-10-12 13:35:32 -0300

    consultando objetos com hibernate

--- src/main/java/com/jordan/algafoods/jpa/ConsultaCozinhaMain.java
+++ src/main/java/com/jordan/algafoods/jpa/ConsultaCozinhaMain.java
@@ -1,8 +1,20 @@
 package com.jordan.algafoods.jpa;
 
+import com.jordan.algafoods.AlgafoodsApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
 public class ConsultaCozinhaMain {
 
     public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodsApplication.class)
+            .web(WebApplicationType.NONE)
+            .run(args);
 
+        applicationContext
+            .getBean(CadastroCozinha.class)
+            .listar()
+            .forEach(cozinha -> System.out.println(cozinha.getNome()));
     }
 }
