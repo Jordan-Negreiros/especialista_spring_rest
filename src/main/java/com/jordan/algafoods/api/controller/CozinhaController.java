4934d718164627a69098add8dbe78c3aa013bdcb
Jordan-Negreiros <jordannegreirossantos@gmail.com>
2021-11-11 21:39:10 -0300

    implementando primeiro endpoint

---
+++ src/main/java/com/jordan/algafoods/api/controller/CozinhaController.java
@@ -0,0 +1,24 @@
+package com.jordan.algafoods.api.controller;
+
+import com.jordan.algafoods.domain.model.Cozinha;
+import com.jordan.algafoods.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+
+}
