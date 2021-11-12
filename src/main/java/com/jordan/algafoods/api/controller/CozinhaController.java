8050d7653b55f4c6ccda612fe6612c456e551a51
Jordan-Negreiros <jordannegreirossantos@gmail.com>
2021-11-11 22:05:29 -0300

    alterando content negotiation

--- src/main/java/com/jordan/algafoods/api/controller/CozinhaController.java
+++ src/main/java/com/jordan/algafoods/api/controller/CozinhaController.java
@@ -3,22 +3,35 @@ package com.jordan.algafoods.api.controller;
 import com.jordan.algafoods.domain.model.Cozinha;
 import com.jordan.algafoods.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(
+    value = "/cozinhas",
+    produces = {
+        MediaType.APPLICATION_JSON_VALUE,
+        MediaType.APPLICATION_XML_VALUE
+    })
 public class CozinhaController {
 
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
+    @GetMapping()
     public List<Cozinha> listar() {
         return cozinhaRepository.listar();
     }
+
+    @GetMapping("/{id}")
+    public Cozinha buscar(@PathVariable("id") Long id) {
+        return cozinhaRepository.buscar(id);
+    }
+
 
 }
