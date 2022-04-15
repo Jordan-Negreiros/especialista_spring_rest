01e1e2af2faa447ecd682173d7991fef6b10f982
jordan-negreiros <jordannegreirossantos@gmail.com>
2022-04-15 18:14:39 -0300

    query methods

--- src/main/java/com/jordan/algafoods/api/controller/TestController.java
+++ src/main/java/com/jordan/algafoods/api/controller/TestController.java
@@ -1,13 +1,13 @@
 package com.jordan.algafoods.api.controller;
 
 import com.jordan.algafoods.domain.model.Cozinha;
+import com.jordan.algafoods.domain.model.Restaurante;
 import com.jordan.algafoods.domain.repository.CozinhaRepository;
+import com.jordan.algafoods.domain.repository.RestauranteRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -16,9 +16,20 @@ import java.util.List;
 public class TestController {
 
     private final CozinhaRepository cozinhaRepository;
+    private final RestauranteRepository restauranteRepository;
 
     @GetMapping("/cozinha/nome/{nome}")
     public List<Cozinha> findCozinhaByName(@PathVariable String nome) {
         return cozinhaRepository.findByNomeContaining(nome);
+    }
+
+    @GetMapping("/restaurante/taxa")
+    public List<Restaurante> findRestaurantePorTaxaFrete(BigDecimal taxa_inicial, BigDecimal taxa_final) {
+        return restauranteRepository.findByTaxaFreteBetween(taxa_inicial, taxa_final);
+    }
+
+    @GetMapping("/restaurante")
+    public List<Restaurante> findRestaurantePorTaxaFrete(String nome, Long cozinhaId) {
+        return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
     }
 }
